3ed75a17838b5a8b1cc375485416e9e90f8b7008
xTOUKAM <houllegatte.tom@gmail.com>
2023-06-18 14:42:59 +0200

    Modification du modifAccount.php

--- src/user/modifAccount.php
+++ src/user/modifAccount.php
@@ -1,97 +1,101 @@
 <?php
     require_once("../config/bdd.php");
         
-    if (isset($_GET["com_mail"])) {
-        if (is_numeric($_GET["com_mail"])) {
-            $mail = $_GET["com_mail"];
-            $requete = "SELECT * FROM compte WHERE com_mail = $mail";
-            $resultat = $connexion->query($requete);
-            $ligne = $resultat->fetch(PDO::FETCH_ASSOC);
+    if (isset($_GET["mail"]) && !empty($_GET["mail"])) {
+        $mail = $_GET["mail"];
+        $requete = "SELECT * FROM compte WHERE com_mail = '$mail'";
+        $resultat = $bdd->query($requete);
+        $ligne = $resultat->fetch(PDO::FETCH_ASSOC);
 
-            if (!empty($ligne)) {
-                $prenom = $ligne["com_prenom"];
-                $nom = $ligne["com_nom"];
-                $mail = $ligne["com_mail"];
-                $password = $ligne["com_mdp"];
-            } else {
-                unset($mail);
-            }
+        if (!empty($ligne)) {
+            $prenom = $ligne["com_prenom"];
+            $nom = $ligne["com_nom"];
+            $mail = $ligne["com_mail"];
+            $password = $ligne["com_mdp"];
+        } else {
+            unset($mail);
         }
     }
 
     if (isset($_POST["com_prenom"])) {
         if (empty($_POST["com_prenom"])) {
-            $prenom_message = "Ce champs doit être complété.";
+            $prenom_message = "Ce champ doit être complété.";
         } elseif (!preg_match("/^[[:alpha:]ÀÂÄÉÈÊËÏÎÔÖÙÛÜŸÇàâäéèêëïîôöùûüÿç\-\s]+$/i", $_POST["com_prenom"])) {
-            $prenom_message = "Ce champs doit contenir que des lettres.";
+            $prenom_message = "Ce champ doit contenir uniquement des lettres.";
         } else {
             $prenom = htmlspecialchars($_POST["com_prenom"]);
         }
     }
     if (isset($_POST["com_nom"])) {
         if (empty($_POST["com_nom"])) {
-            $nom_message = "Ce champs doit être complété.";
+            $nom_message = "Ce champ doit être complété.";
         } elseif (!preg_match("/^[[:alpha:]ÀÂÄÉÈÊËÏÎÔÖÙÛÜŸÇàâäéèêëïîôöùûüÿç\-\s]+$/i", $_POST["com_nom"])) {
-            $nom_message = "Ce champs doit contenir que des lettres.";
+            $nom_message = "Ce champ doit contenir uniquement des lettres.";
+        } else {
+            $nom = htmlspecialchars($_POST["com_nom"]);
         }
-        $nom = htmlspecialchars($_POST["com_nom"]);
     }
     if (isset($_POST["com_mail"])) {
         if (empty($_POST["com_mail"])) {
-            $mail_message = "Ce champs doit être complété.";
-        } elseif (!preg_match("/^[a-zA-Z.]+@[a-zA-Z]+\.[a-zA-Z]+$/i", $_POST["mail"])) {
-            $mail_message = "Entrez une adresse mail valide. Exemple : \"toto@mail.com\"";
+            $mail_message = "Ce champ doit être complété.";
+        } elseif (!preg_match("/^[a-zA-Z.]+@[a-zA-Z]+\.[a-zA-Z]+$/i", $_POST["com_mail"])) {
+            $mail_message = "Entrez une adresse e-mail valide. Exemple : \"toto@mail.com\"";
         } else {
             $mail = htmlspecialchars(strtolower($_POST["com_mail"]));
         }
     }
 
+    if (isset($_POST["com_mdp"])) {
+        if (empty($_POST["com_mdp"])) {
+            $motDePasse_message = "Ce champs doit être complété.";
+        } else {
+            $motDePasse = $_POST["com_mdp"];
+        }
+    }
+
+    if (isset($_POST["com_mdp2"])) {
+        if (empty($_POST["com_mdp2"])) {
+            $motDePasseVerif_message = "Ce champs doit être complété.";
+        } elseif ($_POST["com_mdp2"] != $_POST["com_mdp"]) {
+            $motDePasseVerif_message = "Les mots de passe doivent correspondre.";
+        }
+    }
+
+
     if (isset($prenom) && isset($nom) && isset($mail) && isset($password)) {
-        $requete = "UPDATE compte SET com_nom = '$nom', com_prenom = '$prenom', com_mail = '$mail', com_mdp = '$password'";
-        $connexion->exec($requete);
-        // Revenir à la page précédente
-        header("Location: /website/");
+        $requete = "UPDATE compte SET com_nom = '$nom', com_prenom = '$prenom', com_mail = '$mail', com_mdp = '$password' WHERE com_mail = '$mail'";
+        $bdd->exec($requete);
     }
 ?>
 
-<?php
-    if (!isset($mail)) {
-?>
-
-<!DOCTYPE html>
-    <html lang="en">
+<?php if (isset($mail)) : ?>
+    <!DOCTYPE html>
+    <html lang="fr">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel=stylesheet href="../assets/css/inc.css">
+        <link rel="stylesheet" href="../assets/css/inc.css">
         <title>Modifier compte</title>
     </head>
     <body>
-        <?php
-            require("../includes/header.php");
-        ?>
+        <?php require("../includes/header.php"); ?>
         <div class="container">
-        </div>
             <div class="form-container sign-in-container">
                 <form method="POST" action="">
-                    <h1>Modifier compte</h1>
-                    <input class="inp-form" type="text" id="prenom" name="prenom" placeholder="Prénom" value="<?php echo $prenom; ?>" />
-                    <input class="inp-form" type="text" id="nom" name="nom" placeholder="Nom" value="<?php echo $nom; ?>" />
-                    <input class="inp-form" type="email" id="mail" name="mail" placeholder="Adresse mail" value="<?php echo $mail; ?>" />
-                    <input class="inp-form" type="password" id="password" name="password" placeholder="Mot de passe" value="<?php echo $password; ?>" />
+                    <h1>Modifier mon compte</h1>
+                    <input class="inp-form" type="text" id="com_prenom" name="com_prenom" placeholder="Prénom" value="<?php echo $prenom; ?>" />
+                    <input class="inp-form" type="text" id="com_nom" name="com_nom" placeholder="Nom" value="<?php echo $nom; ?>" />
+                    <input class="inp-form" type="password" id="com_mdp" name="com_mdp" placeholder="Mot de passe"/>
+                    <input class="inp-form" type="password" id="com_mdp2" name="com_mdp2" placeholder="Confirmation du mot de passe"/>
                     <button type="submit" class="btn-form">Modifier</button>
                 </form>
             </div>
         </div>
         <footer>
-            <?php
-                require("../includes/footer.php");
-            ?>
+            <?php require("../includes/footer.php"); ?>
         </footer>
-    <?php
-        } else {
-            echo "<p>Erreur : aucun compte sélectionné.</p>";
-        }
-    ?>
     </body>
-</html>
+    </html>
+<?php else : ?>
+    <p>Erreur : aucun compte sélectionné.</p>
+<?php endif; ?>
