35e36f3f32409b4c7200beab35a03e6a69357448
xTOUKAM <houllegatte.tom@gmail.com>
2023-06-20 10:57:11 +0200

    Modification du header + complet

--- src/includes/header.php
+++ src/includes/header.php
@@ -1,6 +1,6 @@
 <?php
-    $com_admin = isset($_SESSION['com_admin']);
-    $com_mail = isset($_SESSION['com_mail']);
+$com_admin = isset($_SESSION['com_admin']);
+$com_mail = isset($_SESSION['com_mail']);
 ?>
 
 <!DOCTYPE html>
@@ -8,29 +8,39 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel=stylesheet href="../assets/css/header.css">
+        <link rel="stylesheet" href="../assets/css/header.css">
         <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet">
     </head>
     <body>
         <!-- Création de la barre de navigation -->
         <div>
             <ul class="tab">
-                <li><a class="a-header-footer" href="/website/"><i class="lni lni-home"></i></a></li>
-                <?php 
-                    if($com_admin == 1 && $_SESSION['com_admin'] == 1){
-                        echo '<li><a class="a-header-footer" href="/website/src/admin/"><i class="lni lni-laptop"></i></i></a></li>';
-                    }
+                <li><a class="a-header-footer" href="<?php echo getRelativePath('../../index.php'); ?>"><i class="lni lni-home"></i></a></li>
+                <?php
+                if ($com_admin == 1 && $_SESSION['com_admin'] == 1) {
+                    echo '<li><a class="a-header-footer" href="' . getRelativePath('../admin/') . '"><i class="lni lni-laptop"></i></a></li>';
+                }
                 ?>
-                <li><a class="a-header-footer" href="/website/src/user/login.php"><i class="lni lni-user"></i></a></li>
-                <?php 
-                    if($com_mail != ""){
-                        echo '<li><a class="a-header-footer" href="#"><i class="lni lni-image"></i></a></li>';
-                    }
+                <li><a class="a-header-footer" href="<?php echo getRelativePath('../user/login.php'); ?>"><i class="lni lni-user"></i></a></li>
+                <?php
+                if ($com_mail != "") {
+                    echo '<li><a class="a-header-footer" href="#"><i class="lni lni-image"></i></a></li>';
+                }
                 ?>
-                <li><a class="a-header-footer" href="/website/src/others/mail.php"><i class="lni lni-hand"></i></a></li>
+                <li><a class="a-header-footer" href="<?php echo getRelativePath('../others/mail.php'); ?>"><i class="lni lni-hand"></i></a></li>
             </ul>
         </div>
         <!-- Insertion des scripts -->
         <script src="../assets/js/header.js"></script>
+
+        <?php
+        // Fonction pour obtenir le chemin relatif en fonction de l'URI actuelle
+        function getRelativePath($target) {
+            $baseDir = str_replace('\\', '/', __DIR__);
+            $rootDir = str_replace('\\', '/', $_SERVER['DOCUMENT_ROOT']);
+            $relativePath = str_replace($rootDir, '', $baseDir);
+            return $relativePath . '/' . $target;
+        }
+        ?>
     </body>
 </html>
